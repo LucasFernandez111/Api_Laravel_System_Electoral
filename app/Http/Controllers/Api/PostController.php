b45ef5a6fb7c64ee8c240cc6933d6ce16371b914
Lucas <flucasyoel@gmail.com>
2023-11-10 22:34:22 -0300

    add PostController

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function createPost(Request $request)
+    {
+        $request->validate([
+            "title" => "required",
+            "content" => "required",
+            "partido" => "required",
+        ]);
+
+        $user_id = auth()->user()->id;
+
+        $post = new Post();
+        $post->user_id = $user_id;
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->partido = $request->partido;
+
+        $post->save();
+
+        return response()->json(['status' => 'OK', 'message' => 'Post Created successful']);
+    }
+    public function listPost($id)
+    {
+    }
+
+    public function updatePost(Request $request, $id)
+    {
+    }
+
+    public function deletePost($id)
+    {
+    }
+}
