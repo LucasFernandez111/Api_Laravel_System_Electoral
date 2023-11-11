eedbdaa904d50248357d89e1f5209955e45b48a7
Lucas <flucasyoel@gmail.com>
2023-11-10 22:35:13 -0300

    Add auth Db,controller,routes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,35 +9,88 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
-    $request->validate([
-        'name' => ['required'],
-        'email' => ['required'],
-        'password' => ['required']
-    ]);
+    public function register(Request $request)
+    {
+        $request->validate(
+            [
+                "name" => ["required"],
+                "email" => ["required", "email", "unique:users"],
+                "password" => ["required"],
+                "rol" => ["required"]
+            ]
 
-    $user = new User();
-    $user->name = $request->name;
-    $user->email = $request->email;
-    $user->password = Hash::make($request->password);
-    $user->save();
+        );
 
-    return response()->json([
-        "status" => "OK",
-        "message" => "Registro exitoso"
-    ]);
-}
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->rol = $request->rol;
+        $user->save();
 
-    public function login(Request $request){
-
+        return response()->json([
+            "status" => "OK",
+            "message" => "Registro exitoso"
+        ]);
     }
 
-    public function userProfile(){
+    public function login(Request $request)
+    {
+        $request->validate(
+            [
+                "email" => ["required", "email"],
+                "password" => ["required"]
+            ]
+        );
 
+
+        $user = User::where("email", "=", $request->email)->first();
+
+        if (isset($user->id)) {
+
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken("auth_token")->plainTextToken;
+
+                return response()->json([
+                    "status" => 200,
+                    "message" => "Create Token succefull",
+                    "Token" => $token
+                ]);
+            } else {
+                return response()->json([
+                    "status" => 0,
+                    "message" => "Password invalid"
+                ]);
+            }
+        } else {
+            return response()->json([
+                "status" => 0,
+                "message" => "User not register"
+            ]);
+        }
     }
 
-    public function logOut(){
 
+
+    public function userProfile()
+    {
+
+        return response()->json([
+            "status" => 0,
+            "message" => "User Authenticated",
+            "data" => auth()->user()
+        ]);
+    }
+
+    public function logOut()
+    {
+
+
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "status" => 200,
+            "message" => "Session Closed"
+        ]);
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,24 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
-Route::post('register', [UserController::class,'register']);
-Route::post('login', [UserController::class,'login']);
-Route::group(['middleware' => ['auth:sanctum']], function(){
 
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    //Routes of Authentication
     Route::get('user-profile', [UserController::class, 'userProfile']);
     Route::get('logout', [UserController::class, 'logOut']);
+
+    //Routes of Post
+    Route::post('create-post', [PostController::class, 'createPost']);
+    Route::get('list-post', [PostController::class, 'listPost']);
+    Route::put('update-post/{id}', [PostController::class, 'updatePost']);
+    Route::delete('delete-post/{id}', [PostController::class, 'deletePost']);
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
